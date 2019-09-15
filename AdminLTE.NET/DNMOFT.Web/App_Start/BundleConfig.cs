e4116172e3e8ff9ece27e943e95fa2d238572033
Diomedes Domínguez <diomedes.dominguez@gmail.com>
2019-09-14 23:45:35 -0400

    Created the home page.

--- AdminLTE.NET/DNMOFT.Web/App_Start/BundleConfig.cs
+++ AdminLTE.NET/DNMOFT.Web/App_Start/BundleConfig.cs
@@ -8,22 +8,16 @@ namespace DNMOFT.Web
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                      "~/Scripts/jquery-{version}.js",
+                      "~/Scripts/bootstrap.bundle.js",
+                      "~/Scripts/AdminLTE/adminlte.js"
+                      ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/css/fontawesome.css",
+                      "~/Content/css/adminlte.css",
                       "~/Content/site.css"));
         }
     }
